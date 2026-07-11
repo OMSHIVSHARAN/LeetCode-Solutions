6697cfc0cac9fbe5803c34240209b69ef816892f
OM SHIVSHARAN <omshivsharan777@gmail.com>
2026-07-11 18:31:29 +0530

    Time: 19 ms (98.14%), Space: 19.7 MB (90.53%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = [[] for _ in range(n)]
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        visited = [False] * n
+        ans = 0
+
+        for i in range(n):
+            if visited[i]:
+                continue
+
+            stack = [i]
+            visited[i] = True
+
+            nodes = 0
+            degree_sum = 0
+
+            while stack:
+                node = stack.pop()
+                nodes += 1
+                degree_sum += len(graph[node])
+
+                for nei in graph[node]:
+                    if not visited[nei]:
+                        visited[nei] = True
+                        stack.append(nei)
+
+            edges_count = degree_sum // 2
+
+            if edges_count == nodes * (nodes - 1) // 2:
+                ans += 1
+
+        return ans
