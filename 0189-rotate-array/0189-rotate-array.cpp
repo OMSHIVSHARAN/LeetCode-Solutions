b8d5d1485e625a4e978308c5e425b480122e3d07
OM SHIVSHARAN <omshivsharan777@gmail.com>
2026-08-17 23:22:42 +0530

    Time: 12 ms (4.5%), Space: 263.1 MB (6.56%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<int>& arr, int k) {
+        int n = arr.size();
+        k = k % n;
+
+        vector<int> temp(n);
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = arr[i];
+        }
+
+        arr = temp;
+    }
+};
